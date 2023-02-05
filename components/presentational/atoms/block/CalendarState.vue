733f34e5d73224823b4bd37f46ae010cc97d65a8
Yuta Toyokawa <kppg42@gmail.com>
2023-02-05 16:22:02 +0900

    予約ステータスの側まで

---
+++ components/presentational/atoms/block/CalendarState.vue
@@ -0,0 +1,51 @@
+<template lang="pug">
+.calendar-state(:class="state")
+  .symbol {{ stateSymbol }}
+</template>
+
+<script setup>
+const props = defineProps({
+  state: String,
+})
+
+const stateSymbol = computed(() => {
+  let symbol
+  switch(props.state) {
+    case 'ok':
+    case 'work':
+    case 'disabled':
+      symbol = '○'
+      break
+    case 'ng':
+      symbol = '-'
+      break
+    case 'rest':
+      symbol = '×'
+      break
+  }
+  return symbol
+})
+</script>
+
+<style lang="sass" scoped>
+.calendar-state
+  font-size: 12px
+  text-align: center
+  width: 40px
+  aspect-ratio: 1
+  display: flex
+  align-items: center
+  justify-content: center
+  &.ok
+    color: $black
+    background-color: $white
+  &.ng, &.rest
+    color: $black
+    background-color: $grey
+  &.work
+    color: $white
+    background-color: $primary
+  &.disabled
+    color: $white
+    background-color: $hint
+</style>
--- components/presentational/organizms/CalendarReserve.vue
+++ components/presentational/organizms/CalendarReserve.vue
@@ -1,34 +1,65 @@
 <template lang="pug">
 .calendar
   BlockSwitchWeek.mb-line
-  .calendar-header
-    .header-title 開始時間
-    CalendarDate.ms-line(date="05" day="日")
-    CalendarDate.ms-line(date="06" day="月")
-    CalendarDate.ms-line(date="07" day="火")
-    CalendarDate.ms-line(date="08" day="水")
-    CalendarDate.ms-line(date="09" day="木")
-    CalendarDate.ms-line(date="10" day="金")
-    CalendarDate.ms-line(date="11" day="土")
+  .calendar-header.mb-line
+    .column-title.black 開始時間
+    BlockCalendarDate.ms-line(date="05" day="日")
+    BlockCalendarDate.ms-line(date="06" day="月")
+    BlockCalendarDate.ms-line(date="07" day="火")
+    BlockCalendarDate.ms-line(date="08" day="水")
+    BlockCalendarDate.ms-line(date="09" day="木")
+    BlockCalendarDate.ms-line(date="10" day="金")
+    BlockCalendarDate.ms-line(date="11" day="土")
+  .calendar-body.mb-line
+    .column-title 09:00~
+    BlockCalendarState.ms-line(state="ok")
+    BlockCalendarState.ms-line(state="ng")
+    BlockCalendarState.ms-line(state="ng")
+    BlockCalendarState.ms-line(state="ng")
+    BlockCalendarState.ms-line(state="ng")
+    BlockCalendarState.ms-line(state="ng")
+    BlockCalendarState.ms-line(state="ng")
+  .calendar-body.mb-line
+    .column-title 10:00~
+    BlockCalendarState.ms-line(state="ok")
+    BlockCalendarState.ms-line(state="ng")
+    BlockCalendarState.ms-line(state="ng")
+    BlockCalendarState.ms-line(state="ng")
+    BlockCalendarState.ms-line(state="ok")
+    BlockCalendarState.ms-line(state="ok")
+    BlockCalendarState.ms-line(state="ok")
+  .calendar-body.mb-line
+    .column-title 11:00~
+    BlockCalendarState.ms-line(state="ok")
+    BlockCalendarState.ms-line(state="ok")
+    BlockCalendarState.ms-line(state="ok")
+    BlockCalendarState.ms-line(state="ok")
+    BlockCalendarState.ms-line(state="ok")
+    BlockCalendarState.ms-line(state="ok")
+    BlockCalendarState.ms-line(state="ok")
+
 </template>
 
 <script setup>
 import BlockSwitchWeek from '~/components/presentational/molescules/block/SwitchWeek.vue'
-import CalendarDate from '~/components/presentational/atoms/block/CalendarDate.vue';
+import BlockCalendarDate from '~/components/presentational/atoms/block/CalendarDate.vue';
+import BlockCalendarState from '~/components/presentational/atoms/block/CalendarState.vue';
 </script>
 
 <style lang="sass" scoped>
 .calendar
-  .calendar-header
+  .calendar-header, .calendar-body
     width: 100%
     display: flex
-    .header-title
+    .column-title
       font-size: 14px
-      color: $white
-      background-color: $dark
       height: 40px
+      background-color: $white
       display: flex
       flex-grow: 1
       align-items: center
       justify-content: center
+      &.black
+        color: $white
+        background-color: $dark
 </style>

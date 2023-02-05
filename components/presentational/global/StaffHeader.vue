d3447bd9c90a9b7e202d84341723fe6461fbb7e7
Yuta Toyokawa <kppg42@gmail.com>
2023-02-06 00:42:23 +0900

    スタッフのヘッダーを実装

---
+++ components/presentational/global/StaffHeader.vue
@@ -0,0 +1,20 @@
+<template lang="pug">
+header
+  .logo LOGO
+  .menu-toggler
+    img(src="@/assets/images/menu-toggler-white.svg")
+</template>
+
+<script setup>
+</script>
+
+<style lang="sass" scoped>
+header
+  width: 100%
+  color: $white
+  background-color: $dark
+  padding: $block-padding
+  display: flex
+  align-items: center
+  justify-content: space-between
+</style>
--- layouts/staff.vue
+++ layouts/staff.vue
@@ -1,11 +1,11 @@
 <template lang="pug">
 div
-  div staff
+  Header.mb-20
   slot
 </template>
 
 <script setup>
-
+import Header from '@/components/presentational/global/StaffHeader.vue'
 </script>
 
 <style lang="sass" scoped>

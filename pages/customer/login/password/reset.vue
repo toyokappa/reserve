267fd8207fe56dc1e92bf53af0281a5af53d5b42
Yuta Toyokawa <kppg42@gmail.com>
2023-02-07 16:53:24 +0900

    パスワードリセット周りの実装

---
+++ pages/customer/login/password/reset.vue
@@ -0,0 +1,37 @@
+<template lang="pug">
+BlockText.mb-line 新しいパスワードを入力してください。
+form
+  InputField.mb-line(
+    name="password"
+    type="password"
+    labelText="パスワード"
+    validation="required|min:8"
+    required
+  )
+  InputField.mb-10(
+    name="password_confirmed"
+    type="password"
+    labelText="パスワード（確認用）"
+    validation="required|confirmed:@password"
+    required
+  )
+  .button-area
+    PrimaryButton.mb-10(
+      :disabled="!meta.valid"
+      @click.prevent="router.push('/')"
+    ) パスワードを再設定する
+</template>
+
+<script setup>
+import BlockText from '~/components/presentational/molescules/block/Text.vue'
+import InputField from '~~/components/presentational/molescules/form/InputField.vue';
+import PrimaryButton from '~~/components/presentational/atoms/button/Primary.vue';
+
+import { useForm } from 'vee-validate'
+
+const router = useRouter()
+const { meta } = useForm()
+</script>
+
+<style lang="sass" scoped>
+</style>
---
+++ pages/customer/login/password/sendMail.vue
@@ -0,0 +1,19 @@
+<template lang="pug">
+.complete-page
+  CompleteHeader(title="メールを送信しました")
+  .mb-30
+    p ご登録頂いたメールアドレス宛に
+    P パスワード再設定メールを送信しましたので
+    p.mb-20 ご確認をお願いいたします。
+    p メール内のURLをクリックいただき
+    p.mb-20 パスワードを再設定してください。
+    p こちらのページは閉じていただいて
+    p 問題ございません。
+</template>
+
+<script setup>
+import CompleteHeader from '~/components/presentational/atoms/CompleteHeader.vue'
+</script>
+
+<style lang="sass" scoped>
+</style>

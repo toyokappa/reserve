d22ea75845e020f8748da83ee39d869ce243d5b6
Yuta Toyokawa <kppg42@gmail.com>
2023-02-10 00:47:57 +0900

    スタッフパスワード変更の実装

---
+++ pages/staff/password/edit.vue
@@ -0,0 +1,17 @@
+<template lang="pug">
+BlockText.mb-line 新しいパスワードを入力してください。
+FormPassword(
+  buttonText="パスワードを変更する"
+  @submitForm="router.push('/staff')"
+)
+</template>
+
+<script setup>
+import BlockText from '~/components/presentational/molescules/block/Text.vue'
+import FormPassword from '~~/components/presentational/organizms/FormPassword.vue';
+
+const router = useRouter()
+</script>
+
+<style lang="sass" scoped>
+</style>

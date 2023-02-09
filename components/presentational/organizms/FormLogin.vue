f28c1f068c7af357391a9f1218ec5d6dc1bec85b
Yuta Toyokawa <kppg42@gmail.com>
2023-02-10 00:31:06 +0900

    ログインフォームのリファクタ

---
+++ components/presentational/organizms/FormLogin.vue
@@ -0,0 +1,47 @@
+<template lang="pug">
+form
+  InputField.mb-line(
+    name="email"
+    type="email"
+    labelText="メールアドレス"
+    validation=""
+  )
+  InputField.mb-10(
+    name="password"
+    type="password"
+    labelText="パスワード"
+    validation=""
+  )
+  .button-area
+    PrimaryButton.mb-10(
+      @click.prevent="submitForm"
+    ) ログインする
+    HintText(
+      text="パスワードを忘れた方はこちら"
+      centering
+      @click="router.push(resetLink)"
+    )
+</template>
+
+<script setup>
+import InputField from '~~/components/presentational/molescules/form/InputField.vue';
+import PrimaryButton from '~~/components/presentational/atoms/button/Primary.vue';
+import HintText from '~~/components/presentational/atoms/HintText.vue';
+
+import { useForm } from 'vee-validate'
+
+const props = defineProps({
+  resetLink: String,
+})
+
+const router = useRouter()
+const { values } = useForm()
+
+const emits = defineEmits()
+const submitForm = () => {
+  emits('submitForm', values)
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
--- pages/customer/login/index.vue
+++ pages/customer/login/index.vue
@@ -1,33 +1,14 @@
 <template lang="pug">
 BlockText.mb-line ログイン情報を入力してください。
-InputField.mb-line(
-  name="email"
-  type="email"
-  labelText="メールアドレス"
-  validation=""
+FormLogin(
+  resetLink="/login/password"
+  @submitForm="router.push('/')"
 )
-InputField.mb-10(
-  name="password"
-  type="password"
-  labelText="パスワード"
-  validation=""
-)
-.button-area
-  PrimaryButton.mb-10(
-    @click="router.push('/')"
-  ) ログインする
-  HintText(
-    text="パスワードを忘れた方はこちら"
-    centering
-    @click="router.push('/login/password')"
-  )
 </template>
 
 <script setup>
 import BlockText from '~/components/presentational/molescules/block/Text.vue'
-import InputField from '~~/components/presentational/molescules/form/InputField.vue';
-import PrimaryButton from '~~/components/presentational/atoms/button/Primary.vue';
-import HintText from '~~/components/presentational/atoms/HintText.vue';
+import FormLogin from '~~/components/presentational/organizms/FormLogin.vue';
 
 const router = useRouter()
 </script>

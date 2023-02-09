07e223f8677a0efc40f1a0bf2e420c805d26c687
Yuta Toyokawa <kppg42@gmail.com>
2023-02-10 00:20:17 +0900

    パスワードフォームのリファクタ

---
+++ components/presentational/organizms/FormPassword.vue
@@ -0,0 +1,45 @@
+<template lang="pug">
+form
+  InputField.mb-line(
+    name="password"
+    type="password"
+    labelText="パスワード"
+    validation="required|min:8"
+    required
+  )
+  InputField.mb-10(
+    name="password_confirmed"
+    type="password"
+    labelText="パスワード（確認用）"
+    validation="required|confirmed:@password"
+    required
+  )
+  .button-area
+    PrimaryButton.mb-10(
+      :disabled="!meta.valid"
+      @click.prevent="submitForm"
+    ) {{ buttonText }}
+</template>
+
+<script setup>
+import BlockText from '~/components/presentational/molescules/block/Text.vue'
+import InputField from '~~/components/presentational/molescules/form/InputField.vue';
+import PrimaryButton from '~~/components/presentational/atoms/button/Primary.vue';
+
+import { useForm } from 'vee-validate'
+
+const props = defineProps({
+  buttonText: String,
+})
+
+const router = useRouter()
+const { meta, values } = useForm()
+
+const emits = defineEmits()
+const submitForm = () => {
+  emits('submitForm', values)
+}
+</script>
+
+<style lang="sass" scoped>
+</style>
--- pages/customer/login/password/reset.vue
+++ pages/customer/login/password/reset.vue
@@ -1,36 +1,16 @@
 <template lang="pug">
 BlockText.mb-line 新しいパスワードを入力してください。
-form
-  InputField.mb-line(
-    name="password"
-    type="password"
-    labelText="パスワード"
-    validation="required|min:8"
-    required
-  )
-  InputField.mb-10(
-    name="password_confirmed"
-    type="password"
-    labelText="パスワード（確認用）"
-    validation="required|confirmed:@password"
-    required
-  )
-  .button-area
-    PrimaryButton.mb-10(
-      :disabled="!meta.valid"
-      @click.prevent="router.push('/')"
-    ) パスワードを再設定する
+FormPassword(
+  buttonText="パスワードを再設定する"
+  @submitForm="router.push('/')"
+)
 </template>
 
 <script setup>
 import BlockText from '~/components/presentational/molescules/block/Text.vue'
-import InputField from '~~/components/presentational/molescules/form/InputField.vue';
-import PrimaryButton from '~~/components/presentational/atoms/button/Primary.vue';
-
-import { useForm } from 'vee-validate'
+import FormPassword from '~~/components/presentational/organizms/FormPassword.vue';
 
 const router = useRouter()
-const { meta } = useForm()
 </script>
 
 <style lang="sass" scoped>

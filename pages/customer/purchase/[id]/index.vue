7c2084a52c25710ddd49d1e97abbf7e7edab00ad
Yuta Toyokawa <kppg42@gmail.com>
2023-02-16 01:33:49 +0900

    2回目以降の購入はカード情報を入力しないように変更

--- pages/customer/purchase/[id]/index.vue
+++ pages/customer/purchase/[id]/index.vue
@@ -10,53 +10,68 @@ TotalAmount.mb-10(
   :productItemList="product.product_item_list"
   :totalAmount="totalAmount"
 )
-BlockText.mb-line カード情報を入力してください。
-FormCreditCard(
-  buttonText="チケットを購入する"
-  @purchaseTicket="purchaseTicket"
-)
+template(v-if="card_info")
+  BlockText.mb-line カード情報を確認してください。
+  BlockCreditCard.mb-10(
+    :number="card_info.number"
+    :brand="card_info.brand"
+    :expiration="card_info.expiration"
+    :owner="card_info.owner"
+  )
+  .button-area
+    PrimaryButton.mb-10(@click.prevent="purchaseTicket()") チケットを購入する
+template(v-else)
+  BlockText.mb-line カード情報を入力してください。
+  FormCreditCard(
+    buttonText="チケットを購入する"
+    @purchaseTicket="purchaseTicket"
+  )
 .button-area
   DefaultButton.mb-10(@click.prevent="router.push('/purchase')") 戻る
 </template>
 
 <script setup>
-import BlockText from '~/components/presentational/molescules/block/Text.vue'
-import BlockTicket from '~/components/presentational/molescules/block/Ticket.vue'
-import TotalAmount from '~~/components/presentational/organizms/TotalAmount.vue'
-import DefaultButton from '~~/components/presentational/atoms/button/Default.vue';
-import FormCreditCard from '~~/components/presentational/organizms/FormCreditCard.vue';
+import BlockText from "~/components/presentational/molescules/block/Text.vue";
+import BlockTicket from "~/components/presentational/molescules/block/Ticket.vue";
+import TotalAmount from "~~/components/presentational/organizms/TotalAmount.vue";
+import DefaultButton from "~~/components/presentational/atoms/button/Default.vue";
+import PrimaryButton from "~~/components/presentational/atoms/button/Primary.vue";
+import BlockCreditCard from "~/components/presentational/molescules/block/CreditCard.vue";
+import FormCreditCard from "~~/components/presentational/organizms/FormCreditCard.vue";
 
 definePageMeta({
-  middleware: 'customer-auth'
-})
+  middleware: "customer-auth",
+});
 
-const { id } = useRoute().params
-const { product } = await $fetch(`/customer/purchases/${id}`, {
+const { id } = useRoute().params;
+const { product, card_info } = await $fetch(`/customer/purchases/${id}`, {
   baseURL: useRuntimeConfig().public.apiBaseURL,
   headers: {
-    Authorization: useCustomerAuth().getAuth()
+    Authorization: useCustomerAuth().getAuth(),
   },
-})
+});
 
-const totalAmount = product.product_item_list.reduce((sum, item) => sum + item.amount, 0)
-const purchaseTicket = async (token) => {
+const totalAmount = product.product_item_list.reduce(
+  (sum, item) => sum + item.amount,
+  0
+);
+const purchaseTicket = async (token = null) => {
   await $fetch(`/customer/purchases`, {
     baseURL: useRuntimeConfig().public.apiBaseURL,
-    method: 'POST',
+    method: "POST",
     headers: {
-      Authorization: useCustomerAuth().getAuth()
+      Authorization: useCustomerAuth().getAuth(),
     },
     body: {
       purchase: {
         product_set_id: product.id,
         card_token: token,
       },
-    }
-  })
-  router.push('/purchase/complete')
-}
-const router = useRouter()
+    },
+  });
+  router.push("/purchase/complete");
+};
+const router = useRouter();
 </script>
 
-<style lang="sass" scoped>
-</style>
+<style lang="sass" scoped></style>

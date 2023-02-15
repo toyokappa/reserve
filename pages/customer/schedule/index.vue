7cde5235747ce2c83a716558759b1fefbadb1763
Yuta Toyokawa <kppg42@gmail.com>
2023-02-16 00:36:00 +0900

    スケジュール一覧のAPI連携

--- pages/customer/schedule/index.vue
+++ pages/customer/schedule/index.vue
@@ -1,13 +1,13 @@
 <template lang="pug">
 BlockText.mb-line 予約スケジュール
 BlockReservation.mb-line(
-  v-for="reservation in reservationList"
+  v-for="reservation in reservation_list"
   :key="reservation.id"
-  :schedule="`${reservation.scheduleDate} ${reservation.scheduleTime}`"
-  :time="reservation.requireTime"
-  :name="reservation.programName"
+  :schedule="`${reservation.scheduled_date} ${reservation.scheduled_time}`"
+  :time="reservation.required_time"
+  :name="reservation.program_name"
   :trainer="reservation.trainer"
-  @click="router.push(`/schedule/${reservation.id}`)"
+  @click="useRouter().push(`/schedule/${reservation.id}`)"
 )
 </template>
 
@@ -15,13 +15,15 @@ BlockReservation.mb-line(
 import BlockText from '~/components/presentational/molescules/block/Text.vue'
 import BlockReservation from '~/components/presentational/molescules/block/Reservation.vue'
 
-import sampleData from '@/data/sample'
-const { reservationList } = sampleData
-
 definePageMeta({
   middleware: 'customer-auth'
 })
-const router = useRouter()
+const { reservation_list } = await $fetch('/customer/schedules', {
+  baseURL: useRuntimeConfig().public.apiBaseURL,
+  headers: {
+    Authorization: useCustomerAuth().getAuth()
+  },
+})
 </script>
 
 <style lang="sass" scoped>

bfb720bb954ea2664be10af0b61a0c43434c4451
Yuta Toyokawa <kppg42@gmail.com>
2023-02-23 02:13:25 +0900

    クレジットカード情報がない場合のバグの修正

--- pages/customer/creditCard/index.vue
+++ pages/customer/creditCard/index.vue
@@ -1,6 +1,7 @@
 <template lang="pug">
 BlockText.mb-line 利用中のカード
 BlockCreditCard.mb-10(
+  v-if="in_use"
   :number="in_use.number"
   :brand="in_use.brand"
   :expiration="in_use.expiration"
@@ -9,6 +10,7 @@ BlockCreditCard.mb-10(
 BlockText.mb-line 登録済みのカード
 .mb-10
   BlockCreditCard.mb-line(
+    v-if="registered"
     v-for="card in registered"
     :key="card.id"
     :id="card.id"
@@ -47,58 +49,82 @@ const data = reactive(
 );
 const { in_use, registered } = toRefs(data);
 
+const { $toast } = useNuxtApp();
 const addCard = async (token) => {
-  const { new_card } = await $fetch("/customer/card", {
-    baseURL: useRuntimeConfig().public.apiBaseURL,
-    method: "POST",
-    headers: {
-      Authorization: useCustomerAuth().getAuth(),
-    },
-    body: {
-      card: {
-        card_token: token,
+  try {
+    const { new_card } = await $fetch("/customer/card", {
+      baseURL: useRuntimeConfig().public.apiBaseURL,
+      method: "POST",
+      headers: {
+        Authorization: useCustomerAuth().getAuth(),
       },
-    },
-  });
-  registered.value.unshift(new_card);
+      body: {
+        card: {
+          card_token: token,
+        },
+      },
+    });
+    if (new_card.in_use) {
+      in_use.value = new_card;
+      $toast.info("利用中のカードに追加しました");
+    } else {
+      registered.value.unshift(new_card);
+      $toast.info("登録済みのカードに追加しました");
+    }
+  } catch (e) {
+    $toast.error(`追加できませんでした(code: ${e.status})`);
+    throw e;
+  }
 };
 
 const changeMainCard = async (id) => {
-  const { new_registered } = await $fetch("/customer/card", {
-    baseURL: useRuntimeConfig().public.apiBaseURL,
-    method: "PUT",
-    headers: {
-      Authorization: useCustomerAuth().getAuth(),
-    },
-    body: {
-      card: {
-        id,
+  try {
+    const { new_registered } = await $fetch("/customer/card", {
+      baseURL: useRuntimeConfig().public.apiBaseURL,
+      method: "PUT",
+      headers: {
+        Authorization: useCustomerAuth().getAuth(),
       },
-    },
-  });
-  const mainCard = registered.value.find((card) => card.id === id);
-  in_use.value = mainCard;
-  registered.value = new_registered;
+      body: {
+        card: {
+          id,
+        },
+      },
+    });
+    const mainCard = registered.value.find((card) => card.id === id);
+    in_use.value = mainCard;
+    registered.value = new_registered;
+    $toast.info("利用中のカードを変更しました");
+  } catch (e) {
+    $toast.error(`変更できませんでした(code: ${e.status})`);
+    throw e;
+  }
 };
 
 const deleteCard = async (id) => {
   const deleteCheck = await confirm("本当に削除しますか？");
   if (!deleteCheck) return;
 
-  await $fetch("/customer/card", {
-    baseURL: useRuntimeConfig().public.apiBaseURL,
-    method: "DELETE",
-    headers: {
-      Authorization: useCustomerAuth().getAuth(),
-    },
-    body: {
-      card: {
-        id,
+  try {
+    await $fetch("/customer/card", {
+      baseURL: useRuntimeConfig().public.apiBaseURL,
+      method: "DELETE",
+      headers: {
+        Authorization: useCustomerAuth().getAuth(),
       },
-    },
-  });
-  const cardIndex = registered.value.findIndex((card) => card.id === id);
-  registered.value.splice(cardIndex, 1);
+      body: {
+        card: {
+          id,
+        },
+      },
+    });
+    const cardIndex = registered.value.findIndex((card) => card.id === id);
+    registered.value.splice(cardIndex, 1);
+    $toast.info("カードを削除しました");
+  } catch (e) {
+    $toast.info(`削除できませんでした(code: ${e.status})`);
+    throw e;
+  }
 };
 </script>
 

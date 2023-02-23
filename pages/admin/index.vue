08ab262f8924800be3002d1f0ba1df221733aa12
Yuta Toyokawa <kppg42@gmail.com>
2023-02-23 20:32:22 +0900

    adminにプログラム一覧を実装

--- pages/admin/index.vue
+++ pages/admin/index.vue
@@ -1,2 +1,24 @@
 <template lang="pug">
+BlockMenu.mb-line(
+  v-for="menuSection in mypageMenu"
+  :key="menuSection.title"
+  :title='menuSection.title'
+  :menuList="menuSection.menuList"
+)
 </template>
+
+<script setup>
+import BlockMenu from "~/components/presentational/molescules/block/Menu.vue";
+import profileIcon from "~/assets/images/menu-profile.svg";
+import programIcon from "~/assets/images/menu-program.svg";
+
+const mypageMenu = [
+  {
+    title: "管理",
+    menuList: [
+      { name: "トレーナー管理", link: "/admin/staff", icon: profileIcon },
+      { name: "プログラム管理", link: "/admin/program", icon: programIcon },
+    ],
+  },
+];
+</script>
---
+++ pages/admin/program/index.vue
@@ -0,0 +1,22 @@
+<template lang="pug">
+BlockText.mb-line プログラム管理
+BlockProgram.mb-line(
+  v-for="program in program_list"
+  :key="program.id"
+  :time="program.required_time"
+  :name="program.name"
+  :hint="program.description"
+)
+</template>
+
+<script setup>
+import BlockText from "~/components/presentational/molescules/block/Text.vue";
+import BlockProgram from "~/components/presentational/molescules/block/Program.vue";
+
+const { program_list } = await $fetch("/admin/programs", {
+  baseURL: useRuntimeConfig().public.apiBaseURL,
+  headers: {
+    Authorization: useAdminAuth().getAuth(),
+  },
+});
+</script>
